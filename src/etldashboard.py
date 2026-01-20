8aee8d6192dd170223fdd151bb6dd63f46501c26
Sahithi <74001066+Sahithidurgaraju@users.noreply.github.com>
2026-01-20 14:47:59 +0530

    Update etldashboard.py

--- src/etldashboard.py
+++ src/etldashboard.py
@@ -352,8 +352,8 @@ class Datadashboard:
         cleaner = Datacleaner(df_before,csvname)
         data, dedupe_status = cleaner.remove_duplicates(csvname,logger)
         summary_df,quality_score = self.cleaning_score(df_before,df,dedupe_status,csvname,dbname,table_name)
-        df["Before (%)"] = df["Before (%)"].astype(str)
-        df["After (%)"] = df["After (%)"].astype(str)
+        df["Before (%)"] = df["Before (%)"].astype(str).replace("-", np.nan)
+        df["After (%)"] = df["After (%)"].astype(str).replace("-", np.nan)
         st.dataframe(summary_df, width="stretch")
 
         score = summary_df.loc[
